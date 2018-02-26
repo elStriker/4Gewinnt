a114f4fba1427451521e948d188772a993362321
Baumgartner <Baumgartner@TOBI-PC>
2018-02-26 15:28:29 +0100

    TCP Connection v1.0

--- 4G/MainWindow.xaml.cs
+++ 4G/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace _4G
 {
@@ -20,9 +23,39 @@ namespace _4G
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int player;
+        TcpClient client;
+        IPEndPoint localendP;
         public MainWindow()
         {
-            InitializeComponent(); //Tobi war hier
+            InitializeComponent(); //Tobi war hier 26.02.2018
+        }
+
+        private void btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            var remEndP = new IPEndPoint(IPAddress.Loopback, 9898);
+            client = new TcpClient(localendP);
+            client.SendTimeout = 2000;
+            client.ReceiveTimeout = 2000;
+            client.Connect(remEndP);
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                using (var writer = new StreamWriter(stream, Encoding.ASCII, 4000, leaveOpen: true))
+                {
+                    Console.WriteLine(player.ToString() + ";" + btn_send.IsEnabled);
+                }
+                using (var reader = new StreamReader(stream, Encoding.ASCII, true, 4000, leaveOpen: true))
+                {
+                    var teile = reader.ReadLine().Split(';');
+                    player = Convert.ToInt32(teile[0]);
+                    bool enabled = Convert.ToBoolean(teile[1]);
+                    btn_send.IsEnabled = enabled;
+                }
+
+
+            }
         }
     }
 }
---
+++ Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient cu;
+            TcpClient sq;
+            var localEndp = new IPEndPoint(IPAddress.Loopback, 9898);
+            TcpListener listener = new TcpListener(localEndp);
+            listener.Start();
+
+            Console.WriteLine("Server gestartet");
+            while (true)
+            {
+                using (sq = listener.AcceptTcpClient())
+                {
+                    using (cu = listener.AcceptTcpClient())
+                        Send_Rec(cu, sq);
+                }
+            }
+        }
+
+        private static void Send_Rec(TcpClient send, TcpClient rec)
+        {
+            string msg;
+            using (var reader = new StreamReader(rec.GetStream(), Encoding.ASCII, true, 4000, leaveOpen: true))
+            {
+                msg = reader.ReadLine();
+            }
+            using (var writer = new StreamWriter(send.GetStream(), Encoding.ASCII, 4000, leaveOpen: true))
+            {
+                writer.WriteLine(msg);
+            }
+            Send_Rec(rec, send);
+        }
+    }
+    }
+}
